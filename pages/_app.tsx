a603395d6d29acef7b666348faf83d52ca7a19b4
Daniel Young <young256daniel@gmail.com>
2024-03-17 18:27:43 +0100

    Added new vercel statistics support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import 'tailwindcss/tailwind.css'
-import { Analytics } from'@vercel/analytics/react';
-
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 export default function App({ Component, pageProps }: AppProps) {
-  return <>
-    <Component {...pageProps} />
-    <Analytics></Analytics>
-  </>
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics></Analytics>
+    </>
+  );
 }
